f8e34eb8fcceb8a0dd9876ee1637e993dc077b3e
remeal <dglibchenko@mail.ru>
2021-06-12 01:17:30 +0300

    Init commit

--- App.js
+++ App.js
@@ -1,21 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, FlatList, Text, View, StyleSheet, Image } from 'react-native';
 
-export default function App() {
+export default App = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://www.mos.ru/api/newsfeed/v4/frontend/json/ru/afisha?expand=spheres&fields=id,title,label,image,date_from,date_to,kind,free&filter=%7B%22%3C%3Doccurrences.date_from%22:%222021-06-27+23:59:59%22,%22%3E%3Doccurrences.date_from%22:%222021-05-27+00:00:00%22%7D&per-page=9&sort=occurrences.date_to,-occurrences.date_from')
+      .then((response) => response.json())
+      .then((json) => setData(json.items))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      {isLoading ? <ActivityIndicator/> : (
+        <FlatList style={ styles.flat }
+          data={data}
+          keyExtractor={({ id }, index) => id.toString()}
+          renderItem={({ item }) => (
+            <>
+              <Text>{ item.title }</Text>
+              <Image 
+              style={styles.tinyLogo}
+              source = {{ uri:  'https://www.mos.ru' +  item.image.small.src }}
+              />
+            </>
+          )}
+        />
+      )}
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  flat: {
+    height: '100%',
+    padding: 24,
+    backgroundColor: "#61dafb",
+    marginTop: 100
   },
-});
+  container: {
+    paddingTop: 50,
+  },
+  tinyLogo: {
+    width: 50,
+    height: 50,
+  },
+  logo: {
+    width: 66,
+    height: 58,
+  },
+})
